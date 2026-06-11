0dbb35b4febd65b8de5606088987fe04c7fa4e04
revanth58 <revanthk7777@gmail.com>
2026-06-11 11:26:15 +0530

    dcecercer

--- programs/ops/op5.c
+++ programs/ops/op5.c
@@ -2,12 +2,14 @@
 
 int main() {
     int a = 23,b=34;
-
+    
+    // swapping 2 numbers using a temporary variable
     /*int temp;
     temp =a;
     a=b;
     b=temp;*/
 
+    // swapping 2 numbers without using a temporary variable
     a = a+b;
     b = a-b;
     a = a-b;
